88232274ceb9f1d64e42ef9eb068fd1ccdb10738
Henry <henryz25@lakesideschool.org>
2023-05-19 11:03:00 -0700

    basic function outline

--- ApproximatingTrigFunctions.py
+++ ApproximatingTrigFunctions.py
@@ -1,3 +1,51 @@
-import numpy as np
+## Aditya Casturi, Henry Zhu, Honors Precalculus, 19 May 2023
+## A program to evaluate trignometric functions using the polynomial derivate of sin(x)
 
+import numpy as np
 PI = np.pi
+
+"""
+converts the inputted angle value to an equivalent value to within the range of 0 to pi/4
+parameter: inputted angle value
+returns: converted angle value
+"""
+ 
+def convertAngle(angleValue):
+    return convertedValue
+
+"""
+evaluate sine
+parameter: 
+returns: 
+"""
+ 
+def evalSin(placeholder):
+    return placeholder
+
+"""
+gets the actual sine value of the inputted angle value
+parameter: inputted angle value
+returns: converted angle value
+"""
+ 
+def convertAngleFinal(placeholder, placeholder):
+    return placeholder
+
+"""
+evaluate cosine
+parameter: inputted angle value
+returns: converted angle value
+"""
+ 
+def evalCos(placeholder):
+    return placeholder
+
+"""
+evaluate tangent
+parameter: 
+returns: 
+"""
+ 
+def evalTan(placeholder):
+    return placeholder
+
