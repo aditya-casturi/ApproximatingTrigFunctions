63264aea54336ea907d509d75e5ed39788cb5951
Henry <henryz25@lakesideschool.org>
2023-05-23 10:27:35 -0700

    slightly more setup

--- ApproximatingTrigFunctions.py
+++ ApproximatingTrigFunctions.py
@@ -1,4 +1,4 @@
-## Aditya Casturi, Henry Zhu, Honors Precalculus, 19 May 2023
+## Aditya Casturi, Henry Zhu, Honors Precalculus, 26 May 2023
 ## A program to evaluate trignometric functions using the polynomial derivate of sin(x)
 
 import numpy as np
@@ -14,38 +14,43 @@ def convertAngle(angleValue):
     return convertedValue
 
 """
-evaluate sine
-parameter: 
-returns: 
-"""
- 
-def evalSin(placeholder):
-    return placeholder
-
-"""
-gets the actual sine value of the inputted angle value
+evaluate the sine of an angle between 0 to pi/4 using the polynomial approximation of sin(x)
 parameter: inputted angle value
-returns: converted angle value
+returns: sine value of the angle
 """
  
-def convertAngleFinal(placeholder, placeholder):
-    return placeholder
+def evalSin(convertedValue):
+    return sineValue
 
 """
-evaluate cosine
-parameter: inputted angle value
-returns: converted angle value
+gets the actual trig value of the inputted angle value, accounting for angle symmetry 
+parameter: inputted angle value, sine value of the same angle
+returns: a final trig function value
 """
  
-def evalCos(placeholder):
-    return placeholder
+def convertAngleFinal(convertedValue, sine, functionChoice):
+    return finalValue
 
 """
-evaluate tangent
-parameter: 
-returns: 
+evaluate an angle's cosine using its sine
+parameter: sine value
+returns: cosine value
 """
  
-def evalTan(placeholder):
-    return placeholder
+def evalCos(sine):
+    return cosValue
+
+"""
+evaluate an angle's tangent using its sine
+parameter: sine value
+returns: tangent value
+"""
+ 
+def evalTan(sine):
+    return tanValue
+
+print("Choose a function to evaluate: \n1. sin(x) \n2. cos(x) \n3. tan(x)\n\nInput the number of the desired function: ")
+angleVal = 0
+functionChoice = 0
+
 
