4778ca93728d0411ff54ec61eebd8ca2640ee3a8
Henry <henryz25@lakesideschool.org>
2023-05-23 11:01:08 -0700

    Initial first function (it doesn't work fully)

--- ApproximatingTrigFunctions.py
+++ ApproximatingTrigFunctions.py
@@ -11,6 +11,30 @@ returns: converted angle value
 """
  
 def convertAngle(angleValue):
+    while angleValue >= 2*PI:
+        convertedValue = angleValue-(2*PI)
+        if convertedValue < 2*PI:
+            break
+    while angleValue < 0:
+        convertedValue = angleValue+(2*PI)
+        if convertedValue > 0:
+            break
+    if angleValue >= 0 and angleValue < PI/4:
+        convertedValue = angleValue
+    if angleValue > PI/4 and angleValue <= PI/2:
+        convertedValue = PI/2 - angleValue
+    if angleValue > PI/2 and angleValue <= (3*PI)/4:
+        convertedValue = angleValue - PI/2
+    if angleValue > (3*PI)/4 and angleValue <= PI:
+        convertedValue = PI - angleValue
+    if angleValue > PI and angleValue <= (5*PI)/4:
+        convertedValue = angleValue - PI
+    if angleValue > (5*PI)/4 and angleValue <= (3*PI)/2:
+        convertedValue = angleValue - (3*PI)/2
+    if angleValue > (3*PI)/2 and angleValue <= (7*PI)/4:
+        convertedValue = (3*PI)/2 - angleValue 
+    if angleValue > (7*PI)/4 and angleValue < 2*PI:
+        convertedValue = 2*PI - angleValue
     return convertedValue
 
 """
@@ -20,6 +44,7 @@ returns: sine value of the angle
 """
  
 def evalSin(convertedValue):
+    sineValue = 0
     return sineValue
 
 """
@@ -29,6 +54,7 @@ returns: a final trig function value
 """
  
 def convertAngleFinal(convertedValue, sine, functionChoice):
+    finalValue = 0
     return finalValue
 
 """
@@ -38,6 +64,7 @@ returns: cosine value
 """
  
 def evalCos(sine):
+    cosValue = 0
     return cosValue
 
 """
@@ -47,10 +74,15 @@ returns: tangent value
 """
  
 def evalTan(sine):
+    tanValue = 0
     return tanValue
 
-print("Choose a function to evaluate: \n1. sin(x) \n2. cos(x) \n3. tan(x)\n\nInput the number of the desired function: ")
-angleVal = 0
-functionChoice = 0
+print("Choose a function to evaluate: \n1. sin(x) \n2. cos(x) \n3. tan(x)\n")
+functionChoice = int(input("Input the number of the desired function: "))
+while functionChoice > 3 or functionChoice < 1 :
+    functionChoice = int(input("Invalid input; try again: "))
+angleVal = float(input("\nEnter angle value, in radians, to evaluate: "))
+print(convertAngle(angleVal))
+
 
 
